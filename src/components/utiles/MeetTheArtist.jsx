2614e4b696aa23070b9ae3b4cbccba55ade8a04d
anirban2005143a <23je0104@iitism.ac.in>
2024-09-22 21:32:19 +0530

    fgrfgfr

--- src/components/utiles/MeetTheArtist.jsx
+++ src/components/utiles/MeetTheArtist.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
+import '../../index.css'
 
 const MeetTheArtist = () => {
   return (
-    <div id='meetTheArtist'>
-        <div>
-            <div className="img float-end"><img src="/wedding1.jpeg" alt="" /></div>
-            <div className="content">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dignissimos amet adipisci molestiae? Quaerat officia ratione mollitia deserunt possimus, repudiandae architecto illum totam nulla. Suscipit consequatur sed asperiores soluta facilis.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dignissimos amet adipisci molestiae? Quaerat officia ratione mollitia deserunt possimus, repudiandae architecto illum totam nulla. Suscipit consequatur sed asperiores soluta facilis.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dignissimos amet adipisci molestiae? Quaerat officia ratione mollitia deserunt possimus, repudiandae architecto illum totam nulla. Suscipit consequatur sed asperiores soluta facilis.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dignissimos amet adipisci molestiae? Quaerat officia ratione mollitia deserunt possimus, repudiandae architecto illum totam nulla. Suscipit consequatur sed asperiores soluta facilis.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dignissimos amet adipisci molestiae? Quaerat officia ratione mollitia deserunt possimus, repudiandae architecto illum totam nulla. Suscipit consequatur sed asperiores soluta facilis.
-
+    <div id='meetTheArtist' className=' bg-[#BB9AB1] py-24'>
+        <div className='flex md:flex-row flex-col-reverse justify-center items-center md:w-7/12 mx-auto'>
+            <div className="img md:w-1/3 sm:mx-4 mx-2 my-10"><img src="/a/seb-dean-yOSEqRsOAnE-unsplash.jpg" alt="" className=' w-full object-cover object-center' style={{aspectRatio:"1/1"}} /></div>
+            <div className="content md:w-2/3 ">
+                <div className="heading capitalize playfair-display-font w-10/12 mx-auto text-3xl sm:text-start text-center"><span className='border-b-[1px] border-black'>meet the artist</span></div>
+                <div className="text-base  cormorant-garamond-font font-normal w-10/12 mx-auto py-6 ">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi esse, cum ipsa quibusdam ducimus nam obcaecati <br/><br/> eveniet ea sequi consequuntur aliquid eius aspernatur nesciunt quaerat fugiat error repellendus distinctio eum?
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi esse, cum ipsa quibusdam ducimus nam obcaecati eveniet ea sequi consequuntur aliquid eius aspernatur nesciunt quaerat fugiat error repellendus distinctio eum?
+                </div>
             </div>
         </div>
     </div>
--- src/components/utiles/marterPiece.jsx
+++ src/components/utiles/marterPiece.jsx
@@ -6,8 +6,8 @@ const MasterPiece = (props) => {
 
     return (
         <div id='master-piece' className='w-full overflow-auto py-16 bg-[#D2E0FB]'>
-            <div className="title text-[#000000] ">
-                <p className='border-b-[1px] border-black font-semibold text-4xl uppercase cormorant-garamond-font mx-6 text-center'>masterpiece gallery</p> 
+            <div className="title text-[#000000] text-center">
+                <span className='border-b-[1px] border-black font-semibold text-4xl uppercase cormorant-garamond-font mx-auto'>masterpiece</span> 
                 <p className=' md:w-7/12 sm:w-9/12 w-10/12 mx-auto dancing-script-font my-6 text-lg text-center'>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, ullam nemo deserunt iusto fugit totam suscipit harum debitis minus sit cupiditate corporis laborum consequuntur veniam assumenda! Tempore est quo nesciunt!"</p>
             </div>
 
@@ -27,8 +27,8 @@ const MasterPiece = (props) => {
                     <div className="col md:w-2/5  md:h-full p-2 border-2 border-black m-1"><img src="/a/jakub-neskora-Qeyljff0tkA-unsplash.jpg"  alt=""  className='w-full h-full object-cover cursor-pointer object-center'  style={{aspectRatio:"32/27"}} /></div>
                 </div>
                 <div className="row  flex md:flex-row flex-col">
-                    <div className="col md:w-5/1  p-2 border-2 border-black m-1"><img src="/a/pexels-pixabay-268533.jpg" alt="" className=' object-center object-cover cursor-pointer w-full' style={{aspectRatio:"1/1"}} /></div>
-                    <div className="col md:w-7/1  p-2 border-2 border-black m-1"><video src="/product.mp4" className=' w-full object-cover cursor-pointer object-center' autoPlay={true} loop muted  style={{aspectRatio:"7/5"}}></video></div>
+                    <div className="col md:w-5/12  p-2 border-2 border-black m-1"><img src="/a/pexels-pixabay-268533.jpg" alt="" className=' object-center object-cover cursor-pointer w-full' style={{aspectRatio:"1/1"}} /></div>
+                    <div className="col md:w-7/12  p-2 border-2 border-black m-1"><video src="/product.mp4" className=' w-full object-cover cursor-pointer object-center' autoPlay={true} loop muted  style={{aspectRatio:"7/5"}}></video></div>
                 </div>
             </div>
         </div>
